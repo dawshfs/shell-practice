6d5febbaeabe19483ed080748f99625ea24dd95d
ajay <ajay.wlsdyn@gmail.com>
2025-09-23 11:45:44 +0530

    variables

--- 01-hello-world.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-name1=ajay
-name2=vijay
-
-echo "$name1: hi vijay, how are you"
-echo" $name2: hello ajay, iam good"
---
+++ variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+name1=ajay
+name2=vijay
+
+echo "$name1:: hi vijay, how are you"
+echo "$name2:: hello ajay, iam good"
