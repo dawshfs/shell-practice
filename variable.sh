70a79a51119cc4ba42fd7c664bed8350a8583af5
ajay <ajay.wlsdyn@gmail.com>
2025-09-23 11:51:00 +0530

    variables

--- variable.sh
+++ variable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-name1=ajay
-name2=vijay
+name1=$1
+name2=$2
 
-echo "$name1:: hi vijay, how are you"
-echo "$name2:: hello ajay, iam good"
+echo "$name1:: hi $name2, how are you"
+echo "$name2:: hello $name1, iam good"
